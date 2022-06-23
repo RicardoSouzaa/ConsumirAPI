5f0712c48b25ac0370eff87687edba1a9c4e0b54
RIcardo Souza <ryszard696@yahoo.com.br>
2022-06-23 09:31:21 -0300

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ConsumirAPI._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+
+        <div class="jumbotron">
+            <asp:TextBox ID="txtNome" runat="server" Height="23px" Width="172px"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnEnviar" runat="server" Text="Enviar" OnClick="btnEnviar_Click" />
+            &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnAtualizar" runat="server" Text="Atualizar" OnClick="btnAtualizar_Click" />
+            &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnTodos" runat="server" Text="Todos" />
+            &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" />
+            &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnPesquisar" runat="server" Text="Pesquisar" OnClick="btnPesquisar_Click" />
+            &nbsp;&nbsp;
+        <asp:HiddenField ID="HdId" runat="server" />
+        </div>
+
+        <asp:GridView ID="Gridview1" runat="server" AutoGenerateColumns="false" DataKeyNames="Id" OnRowCommand="GridView1_RowCommand" Width="668px">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="Id" />
+                <asp:BoundField DataField="Nome" HeaderText="Nome" />
+                <asp:ButtonField ButtonType="Link" CommandName="Excluir" Text="Excluir" HeaderText="Excluir" />
+                <asp:ButtonField ButtonType="Link" CommandName="Atualizar" Text="Atualizar" HeaderText="Atualizar" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,169 @@
+﻿using LivroAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumirAPI
+{
+    public partial class _Default : Page
+    {
+        private HttpClient client;
+        private Uri usuarioUri;
+
+        public _Default()
+        {
+            if (client == null)
+            {
+                client = new HttpClient();
+                client.BaseAddress = new Uri("https://localhost:44327");
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                getAll();
+            }
+        }
+
+        private void getAll()
+        {
+            //chamando a API pela URl
+            HttpResponseMessage response = client.GetAsync("api/usuario/").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                usuarioUri = response.Headers.Location;
+                var usuarios = response.Content.ReadAsAsync<IEnumerable<Usuario>>().Result;
+
+                Gridview1.DataSource = usuarios;
+                Gridview1.DataBind();
+            }
+            else
+            {
+                Response.Write(response.StatusCode.ToString() + "-" + response.ReasonPhrase);
+            }
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Atualizar")
+            {
+                int _index = int.Parse((string)e.CommandArgument);
+                string _chave = Gridview1.DataKeys[_index]["Id"].ToString();
+                string _nome = Server.HtmlDecode(Gridview1.Rows[_index].Cells[1].Text);
+
+                HdId.Value = _chave;
+                txtNome.Text = _nome;
+                txtNome.Focus();
+            }
+
+            if (e.CommandName == "Excluir")
+            {
+                int _index = int.Parse((string)e.CommandArgument);
+                string _chave = Gridview1.DataKeys[_index]["Id"].ToString();
+
+                Delete(int.Parse(_chave));
+            }
+        }
+
+        private void Delete(int Id)
+        {
+            HttpResponseMessage response = client.GetAsync("api/usuario/" + Id).Result;
+            response = client.DeleteAsync("api/usuario/" + Id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                usuarioUri = response.Headers.Location;
+                Console.WriteLine("Deletado com sucesso!!!");
+            }
+            else
+            {
+                Response.Write(response.StatusCode.ToString() + " - " + response.ReasonPhrase.ToString());
+            }
+            getAll();
+        }
+
+        protected void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            HttpResponseMessage response = client.GetAsync("api/usuario/?name=" + txtNome.Text).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                usuarioUri = response.Headers.Location;
+                var usuarios = response.Content.ReadAsAsync<IEnumerable<Usuario>>().Result;
+
+                Gridview1.DataSource = usuarios;
+                Gridview1.DataBind();
+            }
+            else
+            {
+                Response.Write(response.StatusCode.ToString() + " - " + response.ReasonPhrase);
+            }
+        }
+
+        protected void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            Update(int.Parse(HdId.Value), txtNome.Text);
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            Enviar(txtNome.Text);
+        }
+
+        private string Enviar(string name)
+        {
+            HttpResponseMessage response = client.GetAsync("api/usuario/").Result;
+
+            object data = new
+            {
+                Nome = name,
+            };
+
+            var json = JsonConvert.SerializeObject(data);
+            var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            response = client.PostAsync("api/usuario/", stringContent).Result;
+            string resultContet = response.Content.ReadAsStringAsync().Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                usuarioUri = response.Headers.Location;
+            }
+            else
+            {
+                Response.Write(response.StatusCode.ToString() + " - " + response.ReasonPhrase.ToString());
+            }
+            getAll();
+            return resultContet;
+        }
+
+        private void Update(int _id, string _nome)
+        {
+            var usuarios = new Usuario() { Id = _id, Nome = _nome };
+            HttpResponseMessage response = client.GetAsync("api/usuario/").Result;
+            response = client.PutAsJsonAsync("api/usuario/" + _id, usuarios).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                usuarioUri = response.Headers.Location;
+            }
+            else
+            {
+                Response.Write(response.StatusCode.ToString() + " - " + response.ReasonPhrase.ToString());
+            }
+            getAll();
+        }
+
+        //private void Adicionar(int Id)
+        //{
+        //    HttpResponseMessage response = client.GetAsync("api/usuario" + Id).Result;
+        //    response = client.PutAsync("api/usuario" + Id).Result;
+        //}
+    }
+}
